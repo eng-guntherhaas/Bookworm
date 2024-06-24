bb9814d90ee569970a8eaae14770d0a8d4eac6e2
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-24 11:57:40 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Gunther Masi Haas on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
