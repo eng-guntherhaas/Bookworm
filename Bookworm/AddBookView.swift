fba4e4cbff6dcdb054bca5d07924b0d2547681e7
guntherhaas <guntherhaas.eng@gmail.com>
2024-07-04 16:55:03 +0200

    added book model and add book page

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,62 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Gunther Masi Haas on 04/07/2024.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 5
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+
+                Section("Write a review") {
+                    TextEditor(text: $review)
+
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                }
+
+                Section {
+                    Button("Save") {
+                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(newBook)
+                        dismiss()
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+#Preview {
+    AddBookView()
+}
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -6,16 +6,30 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+           Text("Count: \(books.count)")
+               .navigationTitle("Bookworm")
+               .toolbar {
+                   ToolbarItem(placement: .topBarTrailing) {
+                       Button("Add Book", systemImage: "plus") {
+                           showingAddScreen.toggle()
+                       }
+                   }
+               }
+               .sheet(isPresented: $showingAddScreen) {
+                   AddBookView()
+               }
+       }
     }
 }
 
